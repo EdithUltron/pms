f008374b4e57c2bd129df217b98c6c9896d8361a
Edith <edithultron123@gmail.com>
2022-09-17 15:42:40 +0530

    commit

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# class Contact(models.Model):
+#     firstName = models.CharField("First name", max_length=255, blank = True, null = True)
+#     lastName = models.CharField("Last name", max_length=255, blank = True, null = True)
+#     email = models.EmailField()
+#     phone = models.CharField(max_length=20, blank = True, null = True)
+#     address = models.TextField(blank=True, null=True)
+#     description = models.TextField(blank=True, null=True)
+#     createdAt = models.DateTimeField("Created At", auto_now_add=True)
+
+#     def __str__(self):
+#         return self.firstName
