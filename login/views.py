4f95048d6c713c843d3711101f447ded391daeca
Edith <edithultron123@gmail.com>
2022-10-11 00:43:05 +0530

    login added

--- login/views.py
+++ login/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import logging
 
 def loginaction(request):
+    logging.error("Inside Login")
+    if request.method =="POST" :
+        logging.error(request.POST)
+    #    print(request.POST) 
+
     return render(request,'login/login_page.html')
