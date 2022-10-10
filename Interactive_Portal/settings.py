87da211815314110854f1a450500f7bdaecdebed
Sasi Vardhan Arli <sasivardhan247@gmail.com>
2022-10-10 21:44:36 +0530

    new

--- Interactive_Portal/settings.py
+++ Interactive_Portal/settings.py
@@ -116,7 +116,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# import os
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# import logging
+
+# logging.info("Log message goes here.")
+# logging.error(BASE_DIR)
+# logging.warning("Log message goes here.")
+# logging.error("Log message goes here.")
+# logging.critical("Log message goes here.")
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
