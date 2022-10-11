695a2cb2580d71705ed2ee62d40f35e8bf96e4c8
Edith <edithultron123@gmail.com>
2022-10-12 00:21:41 +0530

    db

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from django import forms
 
@@ -5,17 +6,24 @@ from django import forms
 # Create your models here.
 
 class Register(models.Model):
-    firstName = models.CharField("First name", max_length=255, blank = True, null = True)
-    lastName = models.CharField("Last name", max_length=255, blank = True, null = True)
-    email = models.EmailField()
+    fullName = models.CharField("Full name", max_length=255, blank = True, null = True)
+    roll=models.CharField(max_length=10, blank = True, null = True)
+    email=models.EmailField(max_length=150,unique=True,null=False)
+    password=models.CharField(max_length=200,null=False)
     phone = models.CharField(max_length=20, blank = True, null = True)
-    address = models.TextField(blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    department = models.CharField(null=False)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
 
     def __str__(self):
-        return self.firstName
+        return self.fullName
 
 class RegisterForm(forms.Form):
     class Meta:
         model=Register
+        fields = "__all__"
+        widgets = {
+            'password':forms.PasswordInput()
+        }
+    
+    def __str__(self) -> str:
+        return "Register form"
